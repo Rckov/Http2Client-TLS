e58cad60c10b20e686275cabfed525c13dc2db03
Eren <kurteren07@gmail.com>
2025-04-15 03:10:29 +0300

    Update NativeWindowsMethods.cs

--- src/TlsClient.Core/Helpers/Natives/NativeWindowsMethods.cs
+++ src/TlsClient.Core/Helpers/Natives/NativeWindowsMethods.cs
@@ -7,11 +7,16 @@ namespace TlsClient.Core.Helpers.Natives
 {
     public static class NativeWindowsMethods
     {
+
         [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, EntryPoint = "LoadLibraryW")]
         public static extern IntPtr LoadLibrary([In][MarshalAs(UnmanagedType.LPWStr)] string path);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetProcAddress([In]IntPtr hModule, [In]string procName);
 
         [DllImport("kernel32.dll", SetLastError = true, EntryPoint = "FreeLibrary")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool FreeLibrary([In] IntPtr hLibrary);
+
     }
 }
